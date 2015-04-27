e755b1f16219faff03feacffe061855506604d3e
dsdshcym <dsdshcym@gmail.com>
2015-04-27 21:33:06 +0800

    Add accounts related models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Student(models.Model):
+    """
+    Student information
+    """
+    id = models.CharField(max_length=20, primary_key=True)
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=20)
+
+class Teacher(models.Model):
+    """
+    Teacherr information
+    """
+    id = models.CharField(max_length=20, primary_key=True)
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=20)
+    title = models.CharField(max_length=20)
+
+class Manager(models.Model):
+    """
+    Manager information
+    """
+    id = models.CharField(max_length=20, primary_key=True)
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=20)
