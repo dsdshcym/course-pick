6a8bdb917161dce660e13f6088b5e668cb44fecb
dsdshcym <dsdshcym@gmail.com>
2015-04-27 21:41:33 +0800

    Prepare to make migrations

---
+++ accounts/migrations/0001_initial.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Manager',
+            fields=[
+                ('id', models.CharField(max_length=20, serialize=False, primary_key=True)),
+                ('name', models.CharField(max_length=20)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Student',
+            fields=[
+                ('id', models.CharField(max_length=20, serialize=False, primary_key=True)),
+                ('name', models.CharField(max_length=20)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Teacher',
+            fields=[
+                ('id', models.CharField(max_length=20, serialize=False, primary_key=True)),
+                ('name', models.CharField(max_length=20)),
+                ('title', models.CharField(max_length=20)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
