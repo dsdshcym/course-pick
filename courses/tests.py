0ba56cef1585a818b7e0d556c20c76300ddf4e72
dsdshcym <dsdshcym@gmail.com>
2015-04-29 22:27:19 +0800

    Add first model test for Course

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
-# Create your tests here.
+from courses.models import Course
+
+class CourseModelTest(TestCase):
+
+    def test_saving_and_retriving_courses(self):
+        first_course = Course.objects.create(
+            id='0001',
+            name='first_test_course',
+            score=2.0,
+            max_student_number=50)
+
+        second_course = Course.objects.create(
+            id='0002',
+            name='second_test_course',
+            score=3.0,
+            max_student_number=10)
+
+        saved_courses = Course.objects.all()
+        self.assertEqual(saved_courses.count(), 2)
+
+        first_saved_item = saved_courses[0]
+        second_saved_item = saved_courses[1]
+        self.assertEqual(first_saved_item.name, 'first_test_course')
+        self.assertEqual(second_saved_item.name, 'second_test_course')
