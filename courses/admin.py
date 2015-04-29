16f3beef1a60feb67981f93be4824218a73dcc1a
dsdshcym <dsdshcym@gmail.com>
2015-04-29 21:11:05 +0800

    Make the Courses app modifiable in admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course, Exam, CourseTime
+
+admin.site.register(Course)
+admin.site.register(Exam)
+admin.site.register(CourseTime)
