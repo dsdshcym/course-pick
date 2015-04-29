6ba1c807292b383210972dafa459cb1550c88dc1
dsdshcym <dsdshcym@gmail.com>
2015-04-29 22:16:51 +0800

    Add weekday and course_time choices in CourseTime

--- courses/models.py
+++ courses/models.py
@@ -39,7 +39,32 @@ class CourseTime(models.Model):
     """
     The time a course is schedueled
     """
+    WEEKDAY_CHOICES = (
+        ('Mon', '周一'),
+        ('Tue', '周二'),
+        ('Wed', '周三'),
+        ('Thu', '周四'),
+        ('Fri', '周五'),
+        ('Sat', '周六'),
+        ('Sun', '周日'),
+    )
+    COURSE_TIME_CHOICES = (
+        (1, '第 1 节'),
+        (2, '第 2 节'),
+        (3, '第 3 节'),
+        (4, '第 4 节'),
+        (5, '第 5 节'),
+        (6, '第 6 节'),
+        (7, '第 7 节'),
+        (8, '第 8 节'),
+        (9, '第 9 节'),
+        (10, '第 10 节'),
+        (11, '第 11 节'),
+        (12, '第 12 节'),
+        (13, '第 13 节'),
+        (14, '第 14 节'),
+    )
     course = models.ForeignKey(Course)
-    weekday = models.CharField(max_length=2)
-    begin = models.PositiveSmallIntegerField()
-    end = models.PositiveSmallIntegerField()
+    weekday = models.CharField(max_length=3, choices=WEEKDAY_CHOICES)
+    begin = models.PositiveSmallIntegerField(choices=COURSE_TIME_CHOICES)
+    end = models.PositiveSmallIntegerField(choices=COURSE_TIME_CHOICES)
