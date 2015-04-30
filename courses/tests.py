9ee435c20d4e4031b8e4f8c2282d8e3e78d29321
dsdshcym <dsdshcym@gmail.com>
2015-04-30 21:38:08 +0800

    Add test for delete course

--- courses/tests.py
+++ courses/tests.py
@@ -1,8 +1,24 @@
 from django.test import TestCase
 from django.http import HttpRequest
+from django.core import exceptions
 
 from courses.models import Course
-from courses.views import add_course
+from courses.views import add_course, delete_course
+
+def add_a_new_course_through_request(id, name, college, classroom, score, max_student_number, remark):
+    request = HttpRequest()
+    request.method = 'POST'
+    request.POST = {
+        'id'                 : id,
+        'name'               : name,
+        'college'            : college,
+        'classroom'          : classroom,
+        'score'              : score,
+        'max_student_number' : max_student_number,
+        'remark'             : remark,
+    }
+    response = add_course(request)
+    return response
 
 class CourseModelTest(TestCase):
 
@@ -27,25 +43,42 @@ class CourseModelTest(TestCase):
         self.assertEqual(first_saved_item.name, 'first_test_course')
         self.assertEqual(second_saved_item.name, 'second_test_course')
 
-class AddCourseTest(TestCase):
+class AddCourseViewTest(TestCase):
 
     def test_manager_can_add_a_new_course(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST = {
-            'id'                 : '0001',
-            'name'               : 'test',
-            'college'            : 'CS',
-            'classroom'          : 'Z2101',
-            'score'              : 2.0,
-            'max_student_number' : 20,
-            'remark'             : '',
-        }
-
-        print request.POST
-
-        response = add_course(request)
-
+        response = add_a_new_course_through_request(
+            '0001',
+            'test',
+            'CS',
+            'Z2101',
+            2.0,
+            20,
+            '',
+        )
         self.assertEqual(Course.objects.count(), 1)
         new_course = Course.objects.first()
         self.assertEqual(new_course.name, 'test')
+
+class DeleteCourseViewTest(TestCase):
+    def setUp(self):
+        new_course = Course.objects.create(
+            id='c0001',
+            name='test_course',
+            college='CS',
+            classroom='Z2101',
+            score=2.0,
+            max_student_number=50,
+            remark='',
+        )
+
+    def test_manager_can_delete_a_exists_course(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST = {
+            'id': 'c0001',
+        }
+
+        response = delete_course(request)
+
+        with self.assertRaises(exceptions.ObjectDoesNotExist):
+            Course.objects.get(id='c0001')
