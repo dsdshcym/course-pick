b18d6b38915557a914c3985904b80c7f3e74e0d9
dsdshcym <dsdshcym@gmail.com>
2015-04-30 17:47:24 +0800

    Add new_users permissions settings

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
 
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User, Permission
 
 from accounts.models import Student, Teacher, Manager
+
+STUDENT_PERMISSION = Permission.objects.get(codename='change_course')
+TEACHER_PERMISSION = Permission.objects.filter(codename__endswith='course')
+MANAGER_PERMISSION = Permission.objects.filter(codename__endswith='course')
 
 def register(request):
     if request.method == 'POST':
@@ -14,12 +18,15 @@ def register(request):
 
         if type == Student.user_type:
             new_student = Student.objects.create(id = id, user=new_user, name=name)
+            new_user.user_permissions.add(STUDENT_PERMISSION)
 
         if type == Teacher.user_type:
             title = request.POST['title']
             new_teacher = Teacher.objects.create(id = id, user=new_user, name=name, title=title)
+            new_user.user_permissions = TEACHER_PERMISSION
 
         if type == Manager.user_type:
             new_manager = Manager.objects.create(id = id, user=new_user, name=name)
+            new_user.user_permissions = MANAGER_PERMISSION
 
         return redirect('/')
