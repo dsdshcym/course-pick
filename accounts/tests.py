d3a723478b6ff41a4bdb856169bc75a128051c59
dsdshcym <dsdshcym@gmail.com>
2015-04-30 14:59:21 +0800

    Add three tests for teacher register

--- accounts/tests.py
+++ accounts/tests.py
@@ -7,22 +7,38 @@ from accounts.models import Student, Teacher, Manager
 from accounts.views import register
 
 class RegisterTest(TestCase):
+    test_student_id = 's001'
+    test_student_name = 'test_student'
 
+    test_teacher_id = 't001'
+    test_teacher_name = 'test_teacher'
+    test_teacher_title = 'test_title'
+
+    test_password = '1234'
+
+    def create_register_request(self, id, name, password, type):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST = {
+            'id'       : id,
+            'name'     : name,
+            'password' : password,
+            'type'     : type,
+        }
+        return request
+
+    # Student Tests
     def create_one_student_register_request(self):
-        test_student = Student(
-            id='s001',
-            name='test_stu'
-        )
-        request = create_register_request(
-            test_student.id,
-            test_student.name,
-            '1234',
+        request = self.create_register_request(
+            self.test_student_id,
+            self.test_student_name,
+            self.test_password,
             'student',
         )
-        return test_student, request
+        return request
 
     def test_student_register_create_correct_user(self):
-        test_student, request = self.create_one_student_register_request()
+        request = self.create_one_student_register_request()
 
         response = register(request)
 
@@ -30,23 +46,23 @@ class RegisterTest(TestCase):
         self.assertEqual(user.count(), 1)
 
         first_user = user[0]
-        self.assertEqual(first_user.username, 's001')
-        self.assertTrue(first_user.check_password('1234'))
+        self.assertEqual(first_user.username, self.test_student_id)
+        self.assertTrue(first_user.check_password(self.test_password))
 
     def test_student_register_create_correct_student(self):
-        test_student, request = self.create_one_student_register_request()
+        request = self.create_one_student_register_request()
 
         response = register(request)
 
         student = Student.objects.all()
-        first_student = student[0]
-        self.assertEqual(first_student.id, test_student.id)
-        self.assertEqual(first_student.name, test_student.name)
-
         self.assertEqual(student.count(), 1)
 
+        first_student = student[0]
+        self.assertEqual(first_student.id, self.test_student_id)
+        self.assertEqual(first_student.name, self.test_student_name)
+
     def test_student_register_create_correct_student_user_relation(self):
-        test_student, request = self.create_one_student_register_request()
+        request = self.create_one_student_register_request()
 
         response = register(request)
 
@@ -54,3 +70,48 @@ class RegisterTest(TestCase):
         saved_student = Student.objects.all()[0]
 
         self.assertEqual(saved_student, saved_user.student)
+
+    # Teacher Tests
+    def create_one_teacher_register_request(self):
+        request = self.create_register_request(
+            self.test_teacher_id,
+            self.test_teacher_name,
+            self.test_password,
+            'teacher',
+        )
+        request.POST['title'] = self.test_teacher_title
+        return request
+
+    def test_teacher_register_create_correct_user(self):
+        request = self.create_one_teacher_register_request()
+
+        response = register(request)
+
+        user = User.objects.all()
+        self.assertEqual(user.count(), 1)
+
+        first_user = user[0]
+        self.assertEqual(first_user.username, 't001')
+        self.assertTrue(first_user.check_password(self.test_password))
+
+    def test_teacher_register_create_correct_teacher(self):
+        request = self.create_one_teacher_register_request()
+
+        response = register(request)
+
+        teacher = Teacher.objects.all()
+        self.assertEqual(teacher.count(), 1)
+
+        first_teacher = teacher[0]
+        self.assertEqual(first_teacher.id, self.test_teacher_id)
+        self.assertEqual(first_teacher.name, self.test_teacher_name)
+
+    def test_teacher_register_create_correct_teacher_user_relation(self):
+        request = self.create_one_teacher_register_request()
+
+        response = register(request)
+
+        saved_user = User.objects.all()[0]
+        saved_teacher = Teacher.objects.all()[0]
+
+        self.assertEqual(saved_teacher, saved_user.teacher)
