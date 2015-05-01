404d8ab0cc6b63898d25e63119f0c94d6b6b3997
dsdshcym <dsdshcym@gmail.com>
2015-05-01 10:14:02 +0800

    Add course-teacher-relation when add a new course

--- courses/views.py
+++ courses/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import permission_required
 
+from accounts.models import Teacher
 from courses.models import Course, Exam, CourseTime
 
 # @permission_required('courses.can_add_course')
 def add_course(request):
     if request.method == 'POST':
-        Course.objects.create(
+        new_course = Course.objects.create(
             id                 = request.POST['id'],
             name               = request.POST['name'],
             college            = request.POST['college'],
@@ -15,6 +16,9 @@ def add_course(request):
             max_student_number = request.POST['max_student_number'],
             remark             = request.POST['remark'],
         )
+        teacher_list = request.POST['teacher']
+        for teacher in teacher_list:
+            new_course.teacher.add(teacher)
         return redirect('/')
 
 def delete_course(request):
