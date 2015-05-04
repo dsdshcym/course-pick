dfecf938f460d539f7796db54c78ec70d7a67dd6
dsdshcym <dsdshcym@gmail.com>
2015-05-04 22:37:03 +0800

    Add logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ from accounts.forms import LoginForm
 urlpatterns = [
     url(r'register/', views.register, name='register'),
     url(r'login/', views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/accounts/login/'}, name='logout'),
 ]
