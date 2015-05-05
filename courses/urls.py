b8c400a8ff10ebcb51338ff550f19ed682c4962c
dsdshcym <dsdshcym@gmail.com>
2015-05-05 22:13:10 +0800

    Add teacher_view url

--- courses/urls.py
+++ courses/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     url(r'^delete/$', views.delete_course, name='delete'),
     url(r'^search/', views.search_course, name='search'),
     url(r'^student/$', views.student_view, name='student_view'),
+    url(r'^teacher/$', views.teacher_view, name='teacher_view'),
     url(r'^manager/$', views.manager_view, name='manager_view'),
     url(r'^add/teacher/(?P<course_id>.*)', views.add_course_teacher, name='add_course_teacher'),
     url(r'^add/coursetime/(?P<course_id>.*)', views.add_coursetime, name='add_coursetime'),
